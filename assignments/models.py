385868af9f9e558b777831401f6a8f6b9b2d5d0a
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-18 10:43:37 +0300

    Models for assignments app as prescribed by TTL

--- assignments/models.py
+++ assignments/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
-class Assignments(models.Model):
+class Assignment(models.Model):
     """This class creates a model for Citizen Reporter's reporting assignments."""
-    media_types = ['image','audio','video']
-
+    # Create the list of acceptable media types for the required_media variable
+    MEDIA_CHOICES = (('Image','image'),('Audio','audio'),('Video','video'))
+    
+    # Required Assignment class attributes 
+    created = models.DateTimeField(auto_now_add=True)
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=500)
-    required_media = models.CharField(max_length=5, choices=media_types, default=None)
-    number_of_responset = models.IntegerField()
+    required_media = models.CharField(max_length=5, choices=MEDIA_CHOICES, default=None)
+    number_of_responses = models.IntegerField()
     deadline = models.DateField()
 
+    class Meta:
+        ordering = ('created',)
