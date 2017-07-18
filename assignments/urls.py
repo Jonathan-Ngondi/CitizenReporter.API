8d0211c97ddf8c6cee3fcfba846dafda6acba36b
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-18 10:46:51 +0300

    Urls for the assignments app

---
+++ assignments/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from assignments import views
+
+#URLs for getting all assignments and getting assignments by id
+urlpatterns = [
+    url(r'^assignments$', views.AssignmentList.as_view()),
+    url(r'^assignments/(?P<pk>[0-9]+)$', views.AssignmentDetail.as_view())
+
+]
+
