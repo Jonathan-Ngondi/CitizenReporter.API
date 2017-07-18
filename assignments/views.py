64904873a98066a37aecdfae9a8607ea4b64bf5f
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-18 10:46:16 +0300

    Views for assignments app, allows user to view one as well as all assignments

--- assignments/views.py
+++ assignments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from assignments.models import Assignment
+from rest_framework import generics
+from assignments.serializers import AssignmentSerializer
 
-# Create your views here.
+
+class AssignmentList(generics.ListCreateAPIView):
+    
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
+
+
+class AssignmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """Allows you to view an individual Assignment, update one as well as delete an 
+       an individual assignment.
+    """
+    
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
