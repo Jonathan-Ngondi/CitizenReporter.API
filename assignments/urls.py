c979be2c7887f21fd7ebf73b2b77b52939a2de65
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-19 10:23:49 +0300

    Naming the urls to comply better with REST

--- assignments/urls.py
+++ assignments/urls.py
@@ -3,8 +3,8 @@ from assignments import views
 
 #URLs for getting all assignments and getting assignments by id
 urlpatterns = [
-    url(r'^assignments$', views.AssignmentList.as_view()),
-    url(r'^assignments/(?P<pk>[0-9]+)$', views.AssignmentDetail.as_view())
+    url(r'^assignments$', views.AssignmentList.as_view(), name='list'), 
+    url(r'^assignments/(?P<pk>[0-9]+)$', views.AssignmentDetail.as_view(), name='detail')
 
 ]
 
