35ecc47adc72c897cd7a60148ba3fd29e7991de8
Jeffkungu <Jeffkungu@gmail.com>
2017-07-19 18:16:06 +0300

    Change where model field to use floats for longitude and latitude

--- stories/models.py
+++ stories/models.py
@@ -5,14 +5,14 @@ class Responses(models.Model):
     '''Creates a model containing information about the story/stories submitted by users.'''
     MEDIA_CHOICES = (('Image','image'),('Audio','audio'),('Video','video'))
 
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(null=False, max_length=250)
     why = models.TextField()
     when = models.DateTimeField(auto_now_add=False)
-    where = models.CharField(max_length=500)
+    where = models.DecimalField(max_digits=9, decimal_places=6)
     who = models.TextField()
-    author = models.CharField(max_length=250)
+    author = models.CharField(max_length=250, default="Anonymous")
     author_id = models.CharField(max_length=250)
     media = models.CharField(choices=MEDIA_CHOICES, default=None, max_length=50)
 
