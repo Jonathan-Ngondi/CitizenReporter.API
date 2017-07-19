8ffed5cf7e6177b742d5a474052cb2819bb6dc9f
Jeffkungu <Jeffkungu@gmail.com>
2017-07-19 21:43:35 +0300

    Add owner field to serializer

--- stories/serializers.py
+++ stories/serializers.py
@@ -4,8 +4,11 @@ from .models import Responses
 class ResponsesSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
 
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
 
         model = Responses
-        fields = ('id', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
+        fields = ('id', 'owner', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
+        read_only_fields = ('created')
