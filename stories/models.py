1d06106abffab72ceb3e64048024d615fa9a1287
Jeffkungu <Jeffkungu@gmail.com>
2017-07-19 10:11:20 +0300

    Change fields for Responses model

--- stories/models.py
+++ stories/models.py
@@ -3,17 +3,18 @@ from django.db import models
 
 class Responses(models.Model):
     '''Creates a model containing information about the story/stories submitted by users.'''
+    MEDIA_CHOICES = (('Image','image'),('Audio','audio'),('Video','video'))
 
     id = models.IntegerField(primary_key=True)
-    date = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(null=False, max_length=250)
     why = models.TextField()
     when = models.DateTimeField(auto_now_add=False)
-    # where = 
+    where = models.CharField(max_length=500)
     who = models.TextField()
-    author = models.CharField(null=False, max_length=250)
-    #author_id = models.IntegerField(primary_key=True)
-    #media = 
+    author = models.CharField(max_length=250)
+    author_id = models.CharField(max_length=250)
+    media = models.CharField(choices=MEDIA_CHOICES, default=None, max_length=50)
 
     class Meta:
         ordering = ('created',)
