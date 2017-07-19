5f10b6eb07504579d15fb57feb938296341739f6
Jeffkungu <Jeffkungu@gmail.com>
2017-07-19 10:13:05 +0300

    Add serializer for model

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from models import Responses
+from .models import Responses
 
 class ResponsesSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
     class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+
         model = Responses
         fields = ('id', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
