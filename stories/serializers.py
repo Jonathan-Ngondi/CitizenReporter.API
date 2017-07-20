dac059f7ff2d356ab7c968517e1d9dbd002a7884
Jeffkungu <Jeffkungu@gmail.com>
2017-07-20 16:25:13 +0300

    Delete user model

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Responses
-from django.contrib.auth.models import User
 
 class ResponsesSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
@@ -11,9 +10,3 @@ class ResponsesSerializer(serializers.ModelSerializer):
         model = Responses
         fields = ('id', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
         read_only_fields = ('created', 'id',)
-
-class UserSerializer(serializers.ModelSerializer):
-    '''Serializer to map the User model into JSON format'''
-
-    class Meta:
-        model = User
