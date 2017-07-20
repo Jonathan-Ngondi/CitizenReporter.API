b6e529bdd0942a4014c76a28698d347b7e5d6c9b
Jeffkungu <Jeffkungu@gmail.com>
2017-07-20 13:32:18 +0300

    Add url for User view

--- stories/urls.py
+++ stories/urls.py
@@ -3,8 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from stories import views
 
 urlpatterns = [
-    url(r'^stories/$', views.ResponsesList.as_view(), name="Create"),
-    url(r'^stories/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="Details"),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^stories/$', views.ResponsesList.as_view(), name="create"),
+    url(r'^stories/user/$', views.UserList.as_view(), name="user-list"),
+    url(r'^stories/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="details"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
