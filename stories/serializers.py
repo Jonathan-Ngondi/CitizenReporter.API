c4511bed16f898aabbf99b34d099300771509779
Jeffkungu <Jeffkungu@gmail.com>
2017-07-20 13:29:58 +0300

    Add User model to seializer

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 from .models import Responses
+from django.contrib.auth.models import User
 
 class ResponsesSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
-
-    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
 
         model = Responses
-        fields = ('id', 'owner', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
-        read_only_fields = ('created')
+        fields = ('id', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
+        read_only_fields = ('created', 'id',)
+
+class UserSerializer(serializers.ModelSerializer):
+    '''Serializer to map the User model into JSON format'''
+
+    class Meta:
+        model = User
