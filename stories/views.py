4356dae43f3c4abc475167af3713705680d06350
Jeffkungu <Jeffkungu@gmail.com>
2017-07-20 13:33:37 +0300

    Add User analiytics

--- stories/views.py
+++ stories/views.py
@@ -2,5 +2,32 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import ResponsesSerializer, UserSerializer
+from .models import Responses
+
+class ResponsesList(generics.ListCreateAPIView):
+    '''
+    This class defines creation property of the api.
+    '''
+    queryset = Responses.objects.all()
+    serializer_class = ResponsesSerializer
+
+    def create_story(self, serializer):
+        '''Saves posted stories.'''
+        serializer.save()
+
+class UserList(generics.ListCreateAPIView):
+    '''This class defines get methothod for a specific User'''
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.accounts.all()
+
+class ResponsesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Responses.objects.all()
+    serializer_class = ResponsesSerializer
