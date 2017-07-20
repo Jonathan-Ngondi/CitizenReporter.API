9637bd99c879478b1610ce7b3002714108e99c05
Jeffkungu <Jeffkungu@gmail.com>
2017-07-20 16:37:21 +0300

    Rectify get method for stories

--- stories/urls.py
+++ stories/urls.py
@@ -5,7 +5,7 @@ from stories import views
 urlpatterns = [
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^stories/$', views.ResponsesList.as_view(), name="create"),
-    url(r'^stories/user/$', views.UserList.as_view(), name="user-list"),
+    url(r'^stories/user/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="user-stories"),
     url(r'^stories/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="details"),
 ]
 
