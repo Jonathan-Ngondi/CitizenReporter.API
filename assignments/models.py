e25baa2716a4e07daa2dbf4ba663a5756ec579b5
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-20 11:35:13 +0300

    Added the media_upload field and removed id from the model

--- assignments/models.py
+++ assignments/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
+
 class Assignment(models.Model):
     """This class creates a model for Citizen Reporter's reporting assignments."""
     # Create the list of acceptable media types for the required_media variable
-    MEDIA_CHOICES = (('Image','image'),('Audio','audio'),('Video','video'))
-    
-    # Required Assignment class attributes 
+    MEDIA_CHOICES = (('Image', 'image'),
+                     ('Audio', 'audio'), ('Video', 'video'))
+
+    # Required Assignment class attributes
     created = models.DateTimeField(auto_now_add=True)
-    id = models.IntegerField(primary_key=True, blank=True)
     title = models.CharField(max_length=250)
-    description = models.CharField(max_length=500)
-    required_media = models.CharField(max_length=5, choices=MEDIA_CHOICES, default="Image")
+    description = models.TextField(max_length=500)
+    required_media = models.CharField(
+        max_length=5, choices=MEDIA_CHOICES, default="Image")
+    media_upload = models.FileField(
+        upload_to='uploads/', blank=True)
     number_of_responses = models.IntegerField(blank=True)
     deadline = models.DateField()
     author = models.CharField(max_length=100, default="Anonymous")
-    assignment_location= models.CharField(max_length=100, blank=True)
+    assignment_location = models.CharField(max_length=100, blank=True)
 
     class Meta:
         ordering = ('created',)
