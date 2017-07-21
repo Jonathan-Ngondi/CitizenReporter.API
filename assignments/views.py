4237b8ba8ab30982a9fa7d436cf18283f5290bb3
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-21 07:12:38 +0300

    Pep 8 formatting

--- assignments/views.py
+++ assignments/views.py
@@ -1,18 +1,19 @@
-from assignments.models import Assignment
 from rest_framework import generics
+
+from assignments.models import Assignment
 from assignments.serializers import AssignmentSerializer
 
 
 class AssignmentList(generics.ListCreateAPIView):
-    
+
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
 
 
 class AssignmentDetail(generics.RetrieveUpdateDestroyAPIView):
-    """Allows you to view an individual Assignment, update one as well as delete an 
+    """Allows you to view an individual Assignment, update one as well as delete an
        an individual assignment.
     """
-    
+
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
