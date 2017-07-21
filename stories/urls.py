5add750c41c106e4f2c732845a89db3b836468a6
Jeffkungu <Jeffkungu@gmail.com>
2017-07-21 12:03:55 +0300

    Modify url to handle static media content

--- stories/urls.py
+++ stories/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from stories import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^stories/$', views.ResponsesList.as_view(), name="create"),
     url(r'^stories/user/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="user-stories"),
     url(r'^stories/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="details"),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
