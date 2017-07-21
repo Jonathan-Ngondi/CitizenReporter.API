528d93f8dac6c3e45317a204cc7125c0d36c040f
Jeffkungu <Jeffkungu@gmail.com>
2017-07-21 18:46:21 +0300

    Change url for consitent custom look_up field

--- stories/urls.py
+++ stories/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^stories/$', views.ResponsesList.as_view(), name="create"),
     url(r'^stories/user/(?P<pk>[0-9]+)/$', views.UserStoriesView.as_view(), name="details"),
-    url(r'^stories/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="details"),
+    url(r'^stories/(?P<fb_id>[0-9]+)/$', views.ResponsesDetail.as_view(), name="details"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
