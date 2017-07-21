dbd2a4f4c6895127e6c6a931620d2cb59a8ff7f1
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-21 07:14:54 +0300

    Pep8 formmatting

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+
 from assignments import views
 
-#URLs for getting all assignments and getting assignments by id
+# URLs for getting all assignments and getting assignments by id
 urlpatterns = [
-    url(r'^assignments$', views.AssignmentList.as_view(), name='list'), 
-    url(r'^assignments/(?P<pk>[0-9]+)$', views.AssignmentDetail.as_view(), name='detail')
+    url(r'^assignments$', views.AssignmentList.as_view(), name='list'),
+    url(r'^assignments/(?P<pk>[0-9]+)$',
+        views.AssignmentDetail.as_view(), name='detail')
 
 ]
-
