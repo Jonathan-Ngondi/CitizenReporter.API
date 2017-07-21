352221700986a9fe7e1474c464142d911fc69e14
Jeffkungu <Jeffkungu@gmail.com>
2017-07-21 17:43:33 +0300

    Change model name to Response

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
-from .models import Responses, Media
+from .models import Response, Media
 
-class ResponsesSerializer(serializers.ModelSerializer):
+class ResponseSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
     
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
 
-        model = Responses
+        model = Response
         fields = ('id', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
         read_only_fields = ('created', 'id',)
 
@@ -15,7 +15,7 @@ class FileListSerializer(serializers.Serializer):
      media = serializers.ListField(child=serializers.FileField(max_length=None,use_url=True))
 
      def create(self, validate_data):
-         responses = Responses.objects.latest('created_at')
+         responses = Response.objects.latest('created_at')
          media = validate_data.pop('media')
          for file in media:
              media_files = Media.objects.create(media=file,responses=responses,**validate_data)
@@ -32,7 +32,7 @@ class UserStoriesSerializer(serializers.ModelSerializer):
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
 
-        model = Responses
+        model = Response
         fields = ('id', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
         read_only_fields = ('created', 'id',)
     
