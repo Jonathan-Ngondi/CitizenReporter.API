f3d68e8966c70b1d2f808bdd39981597f8786ad1
Jeffkungu <Jeffkungu@gmail.com>
2017-07-21 12:45:57 +0300

    Add Media and FileListSerializer class to enable multiple files upload

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
-from .models import Responses
+from .models import Responses, Media
 
 class ResponsesSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
-
-    media = serializers.FileField(max_length=None,use_url=True)
     
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
 
         model = Responses
-        fields = ('id', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id', 'media')
+        fields = ('id', 'created', 'title', 'why', 'when', 'where', 'who', 'author', 'author_id')
         read_only_fields = ('created', 'id',)
+
+class FileListSerializer(serializers.Serializer):
+     media = serializers.ListField(child=serializers.FileField(max_length=None,use_url=True))
+
+     def create(self, validate_data):
+         responses = Responses.objects.latest('created_at')
+         media = validate_data.pop('media')
+         for file in media:
+             media_files = Media.objects.create(media=file,responses=responses,**validate_data)
+         return media_files
+
+class MediaSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Media
+        read_only_fields = ("response",)
