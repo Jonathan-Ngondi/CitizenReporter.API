88fda0c0e5bea3c4245f818d0237e0e8cb1e8a30
Jeffkungu <Jeffkungu@gmail.com>
2017-07-21 17:32:43 +0300

    Change url for retrieving user stories

--- stories/urls.py
+++ stories/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^stories/$', views.ResponsesList.as_view(), name="create"),
-    url(r'^stories/user/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="user-stories"),
+    url(r'^stories/user/(?P<pk>[0-9]+)/$', views.UserStoriesView.as_view(), name="details"),
     url(r'^stories/(?P<pk>[0-9]+)/$', views.ResponsesDetail.as_view(), name="details"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
