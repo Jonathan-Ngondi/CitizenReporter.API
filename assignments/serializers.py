a8069dc9ab502f796109872fc3b33403cee5a7f6
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-07-21 07:05:21 +0300

    Add read_only_field, num_of_responses

--- assignments/serializers.py
+++ assignments/serializers.py
@@ -5,7 +5,9 @@ from assignments.models import Assignment
 
 class AssignmentSerializer(serializers.ModelSerializer):
     """This is a serializer for the Assignment model"""
+
     class Meta:
         model = Assignment
         fields = ('id', 'title', 'description', 'required_media', 'media_upload',
                   'number_of_responses', 'deadline', 'author', 'assignment_location')
+        read_only_fields = ('number_of_responses',)
