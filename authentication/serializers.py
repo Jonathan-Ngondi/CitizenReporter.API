8e94b88fd51f22dc0b63d00c8eb4ef9e385b6cbe
Phillip Ahereza <pahereza@gmail.com>
2017-08-01 16:47:15 +0300

    fixed posting blank fcm_token

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -8,9 +8,6 @@ class ProfileCreateSerializer(serializers.ModelSerializer):
     fb_id = serializers.CharField(required=True)
     profile_pic = serializers.CharField(required=True)
 
-    # make the default blank incase phone hasn't registerd fcm after login
-    fcm_token = serializers.CharField(default='')
-
     class Meta:
         model = ReporterProfile
         fields = ('name', 'profile_pic', 'fb_id', 'fcm_token')
--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from authentication.views import RegisterProfileView, UpdateFCMView, ListUsers
 
 urlpatterns = [
-    url(r'^register', RegisterProfileView.as_view(), name="register-profile"),
+    url(r'^register$', RegisterProfileView.as_view(), name="register-profile"),
     url(r'^$', ListUsers.as_view(), name="list"),
     url(r'^update/(?P<fb_id>\w+)/$', UpdateFCMView.as_view(),
         name="update-fcm")
